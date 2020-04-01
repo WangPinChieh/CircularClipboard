70a52b8d29ea7d9682fdf3f5440e02371f6cb660
Jay <jay.wang@titansoft.com.sg>
2020-04-01 09:22:58 +0800

    init commit

---
+++ CircularClipboard/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Input;
+namespace CircularClipboard
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+		}
+	}
+}
